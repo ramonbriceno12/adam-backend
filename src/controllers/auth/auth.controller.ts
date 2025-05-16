34f35ae8e0a567cd66ebac4e87866282433e44d1
Ramón Briceño <contacto@talentiave.com>
2025-05-16 10:44:59 -0400

    Login and register functional

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -6,7 +6,7 @@ import { db } from '../../config/db';
 const JWT_SECRET = process.env.JWT_SECRET || 'secret'; // You should use .env in real case
 
 export const register = async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, type } = req.body;
 
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -17,8 +17,8 @@ export const register = async (req: Request, res: Response) => {
     }
 
     const result = await db.query(
-      'INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3) RETURNING id, name, email',
-      [name, email, hashedPassword]
+      'INSERT INTO users (name, email, password_hash, user_type) VALUES ($1, $2, $3, $4) RETURNING id, name, email, user_type',
+      [name, email, hashedPassword, type]
     );
 
     res.status(201).json({ user: result.rows[0] });
@@ -29,13 +29,10 @@ export const register = async (req: Request, res: Response) => {
 
 export const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
-
   try {
     const userRes = await db.query('SELECT * FROM users WHERE email = $1', [email]);
     const user = userRes.rows[0];
-
     if (!user) return res.status(401).json({ message: 'Invalid credentials' });
-
     const match = await bcrypt.compare(password, user.password_hash);
     if (!match) return res.status(401).json({ message: 'Invalid credentials' });
 
